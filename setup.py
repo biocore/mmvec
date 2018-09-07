9a352d8672fc36ec279fe1cce59b8f302a0b7df4
Jamie Morton <jamietmorton@gmail.com>
2018-09-07 15:46:19 -0700

    BLD: fixing setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open('README.md') as f:
 # version parsing from __init__ pulled from Flask's setup.py
 # https://github.com/mitsuhiko/flask/blob/master/setup.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
-with open('songbird/__init__.py', 'rb') as f:
+with open('maestro/__init__.py', 'rb') as f:
     hit = _version_re.search(f.read().decode('utf-8')).group(1)
     version = str(ast.literal_eval(hit))
 
