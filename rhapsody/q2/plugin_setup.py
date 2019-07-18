264c3e437517a886fa6aab23612cde251881f50d
nbokulich <nbokulich@gmail.com>
2019-07-18 11:58:09 -0500

    just a couple typos and stylistic fixes

--- rhapsody/q2/plugin_setup.py
+++ rhapsody/q2/plugin_setup.py
@@ -23,10 +23,10 @@ plugin = qiime2.plugin.Plugin(
     name='rhapsody',
     version=__version__,
     website="https://github.com/mortonjt/rhapsody",
-    short_description=('Plugin for performing microbe-metabolite '
-                       'co-occurence analysis.'),
-    description=('This is a QIIME 2 plugin supporting microbe-metabolite '
-                 'co-occurence analysis using mmvec.'),
+    short_description='Plugin for performing microbe-metabolite '
+                      'co-occurence analysis.',
+    description='This is a QIIME 2 plugin supporting microbe-metabolite '
+                'co-occurence analysis using mmvec.',
     package='rhapsody')
 
 plugin.methods.register_function(
@@ -60,31 +60,29 @@ plugin.methods.register_function(
     },
     parameter_descriptions={
         'metadata': 'Sample metadata table with covariates of interest.',
-        'training_column': ("The metadata column specifying which "
-                            "samples are for training/testing. "
-                            "Entries must be marked `Train` for training "
-                            "examples and `Test` for testing examples. "),
-        'num_testing_examples': ("The number of random examples to select "
-                                 "if `training_column` isn't specified"),
-        'epochs': ('The number of total number of iterations '
-                   'over the entire dataset'),
-        'batch_size': ('The number of samples to be evaluated per '
-                       'training iteration'),
-        'input_prior': ('Width of normal prior for the microbial '
-                        'coefficients .Smaller values will regularize '
+        'training_column': "The metadata column specifying which "
+                           "samples are for training/testing. "
+                           "Entries must be marked `Train` for training "
+                           "examples and `Test` for testing examples. ",
+        'num_testing_examples': "The number of random examples to select "
+                                "if `training_column` isn't specified.",
+        'epochs': 'The total number of iterations over the entire dataset.',
+        'batch_size': 'The number of samples to be evaluated per '
+                      'training iteration.',
+        'input_prior': 'Width of normal prior for the microbial '
+                       'coefficients. Smaller values will regularize '
+                       'parameters towards zero. Values must be greater '
+                       'than 0.',
+        'output_prior': 'Width of normal prior for the metabolite '
+                        'coefficients. Smaller values will regularize '
                         'parameters towards zero. Values must be greater '
-                        'than 0.'),
-        'output_prior': ('Width of normal prior for the metabolite '
-                         'coefficients. Smaller values will regularize '
-                         'parameters towards zero. Values must be greater '
-                         'than 0.'),
-        'learning_rate': ('Gradient descent decay rate.'),
-
+                        'than 0.',
+        'learning_rate': 'Gradient descent decay rate.'
     },
     name='Microbe metabolite vectors',
-    description=("Performs bi-loglinear multinomial regression and calculates "
-                 "the conditional probability ranks of metabolite "
-                 "co-occurence given the microbe presence."),
+    description="Performs bi-loglinear multinomial regression and calculates "
+                "the conditional probability ranks of metabolite "
+                "co-occurence given the microbe presence.",
     citations=[]
 )
 
