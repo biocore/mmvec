f40b73980a9921379a10c69f99f426986f37503d
Jamie Morton <jmorton@eng.ucsd.edu>
2019-08-22 11:11:01 -0400

    wtf

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 # ----------------------------------------------------------------------------
 # Copyright (c) 2016--, gneiss development team.
-n#
+#
 # Distributed under the terms of the Modified BSD License.
 #
 # The full license is in the file COPYING.txt, distributed with this software.
