98565911b01b7c987d88d684f90802423715ce18
Jamie Morton <jmorton@eng.ucsd.edu>
2019-08-22 11:12:13 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(name='mmvec',
           'scipy >= 0.15.1',
           'nose >= 1.3.7',
           'scikit-bio >= 0.5.1',
-          'pytorch',
+          'torch',
           'tensorboardX',
           'tqdm'
       ],
