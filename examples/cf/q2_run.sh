46ee2e58c3b4a6259dc09766c970bfb4bc5cb902
Jamie Morton <jamietmorton@gmail.com>
2019-09-05 10:50:54 -0400

    Converting all csvs to txts

--- examples/cf/q2_run.sh
+++ examples/cf/q2_run.sh
@@ -17,3 +17,16 @@ qiime emperor biplot \
       --p-number-of-features 50 \
       --o-visualization emperor.qzv \
       --p-ignore-missing-samples
+
+qiime mmvec heatmap \
+      --i-ranks ranks.qza \
+      --o-visualization heatmap.qzv
+
+mmvec paired-omics \
+      --microbe-file otus_nt.biom \
+      --metabolite-file lcms_nt.biom  \
+      --epochs 100 \
+      --learning-rate 1e-3 \
+      --summary-dir testing
+
+qiime tools import --input-path testing/latent_dim_3_input_prior_1.00_output_prior_1.00_beta1_0.90_beta2_0.95_ranks.txt --output-path ranks.qza --type FeatureData[Conditional]
