f1df6be331746254b6ab9c8ff402604ce8b0a4ec
nbokulich <nbokulich@gmail.com>
2019-09-12 11:39:37 -0700

    removed detritus from rhapsody->mmvec move

--- rhapsody/q2/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._stats import Conditional, ConditionalDirFmt, ConditionalFormat
-from ._method import mmvec
-from ._visualizers import heatmap, paired_heatmap
-
-
-__all__ = ['mmvec', 'Conditional', 'ConditionalFormat', 'ConditionalDirFmt',
-           'heatmap', 'paired_heatmap']
