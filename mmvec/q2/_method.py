88d5dbc431b47969efe23ae13073239b19cc58e0
Keegan Evans <keegan.evans@gmail.com>
2022-05-25 14:20:54 -0700

    IMP: q2 paired-omics method wired up

--- mmvec/q2/_method.py
+++ mmvec/q2/_method.py
@@ -13,7 +13,7 @@ from scipy.sparse.linalg import svds
 
 def paired_omics(microbes: biom.Table,
                  metabolites: biom.Table,
-                 metadata: Metadata = None,
+                 metadata: qiime2.Metadata = None,
                  training_column: str = None,
                  num_testing_examples: int = 5,
                  min_feature_count: int = 10,
@@ -26,8 +26,8 @@ def paired_omics(microbes: biom.Table,
                  equalize_biplot: float = False,
                  arm_the_gpu: bool = False,
                  summary_interval: int = 60) -> (
-                     pd.DataFrame, OrdinationResults, qiime2.Metadata
-                 ):
+                         pd.DataFrame, OrdinationResults, qiime2.Metadata
+                         ):
 
     if metadata is not None:
         metadata = metadata.to_dataframe()
@@ -54,7 +54,6 @@ def paired_omics(microbes: biom.Table,
     train_microbes_coo = coo_matrix(train_microbes_df.values)
     test_microbes_coo = coo_matrix(test_microbes_df.values)
 
-    #with tf.Graph().as_default(), tf.Session() as session:
     model = MMvecALR(
         microbes=microbes,
         metabolites= metabolites,
@@ -62,63 +61,19 @@ def paired_omics(microbes: biom.Table,
         sigma_u=input_prior, sigma_v=output_prior,
         )
 
-    mmvec_training_loop(model=model, learning_rate=learning_rate, epochs=epochs, batch_size=batch_size, summary_interval=summary_interval)
+    convergence_stats = pd.DataFrame.from_records(mmvec_training_loop(model=model,
+        learning_rate=learning_rate, epochs=epochs, batch_size=batch_size,
+        summary_interval=summary_interval)
+        ,
+        columns=['iteration','loss', 'cross-validation'])
+
+
+    convergence_stats.astype({'loss': 'float', 'cross-validation':
+        'float'}, copy=False)
+
+    convergence_stats.set_index("iteration", inplace=True)
+    convergence_stats.index.name="id"
+
+    biplot = model.get_ordination()
     ranks = model.ranks_dataframe()
-    #ranks = pd.DataFrame(model.ranks(), index=train_microbes_df.columns,
-    #                     columns=train_metabolites_df.columns)
-    if latent_dim > 0:
-        u, s, v = svds(ranks - ranks.mean(axis=0), k=latent_dim)
-    else:
-        # fake it until you make it
-        u, s, v = svds(ranks - ranks.mean(axis=0), k=1)
-
-    ranks = ranks.T
-    ranks.index.name = 'featureid'
-    s = s[::-1]
-    u = u[:, ::-1]
-    v = v[::-1, :]
-    if equalize_biplot:
-        microbe_embed = u @ np.sqrt(np.diag(s))
-        metabolite_embed = v.T @ np.sqrt(np.diag(s))
-    else:
-        microbe_embed = u @ np.diag(s)
-        metabolite_embed = v.T
-
-    pc_ids = ['PC%d' % i for i in range(microbe_embed.shape[1])]
-    features = pd.DataFrame(
-        microbe_embed, columns=pc_ids,
-        index=train_microbes_df.columns)
-    samples = pd.DataFrame(
-        metabolite_embed, columns=pc_ids,
-        index=train_metabolites_df.columns)
-    short_method_name = 'mmvec biplot'
-    long_method_name = 'Multiomics mmvec biplot'
-    eigvals = pd.Series(s, index=pc_ids)
-    proportion_explained = pd.Series(s**2 / np.sum(s**2), index=pc_ids)
-    biplot = OrdinationResults(
-        short_method_name, long_method_name, eigvals,
-        samples=samples, features=features,
-        proportion_explained=proportion_explained)
-
-    its = np.arange(len(loss))
-    convergence_stats = pd.DataFrame(
-        {
-            'loss': loss,
-            'cross-validation': cv,
-            'iteration': its
-        }
-    )
-
-    convergence_stats.index.name = 'id'
-    convergence_stats.index = convergence_stats.index.astype(np.str)
-
-    c = convergence_stats['loss'].astype(np.float)
-    convergence_stats['loss'] = c
-
-    c = convergence_stats['cross-validation'].astype(np.float)
-    convergence_stats['cross-validation'] = c
-
-    c = convergence_stats['iteration'].astype(np.int)
-    convergence_stats['iteration'] = c
-
     return ranks, biplot, qiime2.Metadata(convergence_stats)
--- mmvec/train.py
+++ mmvec/train.py
@@ -8,16 +8,35 @@ def mmvec_training_loop(model, learning_rate, batch_size, epochs,
         batch_iterations = int(model.nnz / batch_size)
 
         for batch in range(batch_iterations):
+            #iteration = epoch*batch_iterations + batch + 1
+            iteration = epoch * model.nnz // batch_size
 
-            draws = torch.multinomial(model.microbe_relative_freq,
-                                      batch_size,
-                                      replacement=True).T
+            draws = torch.multinomial(
+                        model.microbe_relative_freq(model.microbes_train),
+                        batch_size,
+                        replacement=True).T
 
-            mmvec_model = model(draws)
+            loss = model(draws, model.metabolites_train)
 
             optimizer.zero_grad()
-            mmvec_model.backward()
+            loss.backward()
             optimizer.step()
 
+
+
+
+
+
         if epoch % summary_interval == 0:
-            print(f"loss: {mmvec_model.item()}\nBatch #: {epoch}")
+
+            with torch.no_grad():
+                cv_draw = torch.multinomial(
+                        model.microbe_relative_freq(model.microbes_test),
+                        batch_size,
+                        replacement=True).T
+                cv_loss = model(cv_draw, model.metabolites_test)
+                yield (str(iteration), loss.item(), cv_loss.item())
+
+        else:
+            yield (str(iteration), loss.item(), None)
+
